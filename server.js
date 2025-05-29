39d0ecf64d43b9413c2be5aeb2c515b7cb918fe3
sathish10162000 <Sathish.k@ikomet.com>
2025-05-29 15:40:08 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello from CI/CD Docker App!'));
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
